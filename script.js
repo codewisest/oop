19edc02415e7f1533a357c1fa711360ceaa6b0ec
Chijioke <wisestcodes@gmail.com>
2023-07-21 20:11:39 -0400

    prototypal inheritance

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 'use strict';
-const Person = function (firstName, birthyear) {
-  this.firstName = firstName;
-  this.birthyear = birthyear;
+// const Person = function (firstName, birthyear) {
+//   this.firstName = firstName;
+//   this.birthyear = birthyear;
+// };
+
+// const chiji = new Person('Chiji', 1987);
+// console.log(chiji);
+
+// const gloria = new Person('Gloria', 1994);
+// console.log(gloria);
+
+// // prototypes
+// console.log(Person.prototype);
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthyear);
+// };
+
+// Person.prototype.species = 'Homo Sapien';
+// chiji.calcAge();
+// console.log(chiji.__proto__);
+// console.log(chiji.species);
+
+const Jesus = function (name, title) {
+  this.name = name;
+  this.title = title;
 };
 
-const chiji = new Person('Chiji', 1987);
-console.log(chiji);
+Jesus.prototype.righteous = true;
 
-const gloria = new Person('Gloria', 1994);
-console.log(gloria);
+// If any man be in Christ, he is a new creation
+const chijioke = new Jesus('Chijioke', 'Child of God');
 
-// prototypes
-console.log(Person.prototype);
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthyear);
-};
-
-Person.prototype.species = 'Homo Sapien';
-chiji.calcAge();
-console.log(chiji.__proto__);
-console.log(chiji.species);
+console.log(chijioke.hasOwnProperty('righteousness')); // returns false
+console.log(chijioke.righteous); // returns true
