ed09a9d56d5b92f7398cd6c49f6245cd6cb9ce00
Chijioke <wisestcodes@gmail.com>
2023-07-24 23:18:36 -0400

    accelerate and brake car

---
+++ codechallenge_1.js
@@ -0,0 +1,23 @@
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const bmw = new Car('BMW', 120);
+const benz = new Car('Mercedes', 95);
+
+bmw.accelerate();
+benz.accelerate();
+
+bmw.brake();
+benz.brake();
