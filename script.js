aa7c07e2e4a4af4884b9c717669745119f821bd2
Chijioke <wisestcodes@gmail.com>
2023-07-24 23:00:59 -0400

    built in objects prototypal inheritance

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 'use strict';
-// const Person = function (firstName, birthyear) {
-//   this.firstName = firstName;
-//   this.birthyear = birthyear;
-// };
+const Person = function (firstName, birthyear) {
+  this.firstName = firstName;
+  this.birthyear = birthyear;
+};
 
-// const chiji = new Person('Chiji', 1987);
-// console.log(chiji);
+const chiji = new Person('Chiji', 1987);
+console.log(chiji);
 
-// const gloria = new Person('Gloria', 1994);
-// console.log(gloria);
+const gloria = new Person('Gloria', 1994);
+console.log(gloria);
 
-// // prototypes
-// console.log(Person.prototype);
-// Person.prototype.calcAge = function () {
-//   console.log(2037 - this.birthyear);
-// };
+// prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthyear);
+};
 
-// Person.prototype.species = 'Homo Sapien';
-// chiji.calcAge();
-// console.log(chiji.__proto__);
-// console.log(chiji.species);
+Person.prototype.species = 'Homo Sapien';
+chiji.calcAge();
+console.log(chiji.__proto__);
+console.log(chiji.species);
 
 const Jesus = function (name, title) {
   this.name = name;
@@ -33,3 +33,20 @@ const chijioke = new Jesus('Chijioke', 'Child of God');
 
 console.log(chijioke.hasOwnProperty('righteousness')); // returns false
 console.log(chijioke.righteous); // returns true
+
+// prototype on built-in objects
+// top of prototype chain
+console.log(chiji.__proto__.__proto__);
+
+const arr = [3, 6, 4, 5, 9, 3, 6, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
