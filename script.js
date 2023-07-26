3f64f4c3523a617948f9f25b89e724d1aff2a3a7
Chijioke <wisestcodes@gmail.com>
2023-07-25 22:22:32 -0400

    object getter and setter

--- script.js
+++ script.js
@@ -70,3 +70,21 @@ const chizoba = new PersonCl('Chizoba', 1994);
 chizoba.calcAge();
 console.log(chizoba);
 chizoba.greet();
+
+// getters and setters
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(movement) {
+    this.movements.push(movement);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
