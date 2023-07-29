ef65c56bebbfa372cda4ccfba537ced47436ccad
Chijioke <wisestcodes@gmail.com>
2023-07-29 09:08:41 -0400

    object dot create student inheritance

--- script.js
+++ script.js
@@ -188,3 +188,20 @@ const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1989);
 
 sarah.calcAge();
+
+// student inherit
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
