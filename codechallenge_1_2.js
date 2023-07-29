e6103445a6f2b7a29b4ed99d349c7fcc12c7f2e3
Chijioke <wisestcodes@gmail.com>
2023-07-28 20:58:40 -0400

    create electric car

--- codechallenge_1_2.js
+++ codechallenge_1_2.js
@@ -51,3 +51,33 @@ const ford = new CarCl('Ford', 120);
 console.log(ford.speedUs);
 ford.speedUs = 50;
 console.log(ford);
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Code challenge 3
+const CarEV = function (make, speed, batteryLevel) {
+  Car.call(this, make, speed);
+  this.batteryLevel = batteryLevel;
+};
+
+CarEV.prototype = Object.create(Car.prototype);
+
+CarEV.prototype.chargeBattery = function (chargeTo) {
+  this.batteryLevel += chargeTo;
+};
+
+CarEV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.batteryLevel -= 1;
+  console.log(
+    `Tesla going going at ${this.speed} Km/h with a charge of ${this.batteryLevel}`
+  );
+};
+
+const tesla = new CarEV('Tesla', 120, 80);
+
+tesla.chargeBattery(90);
+tesla.accelerate();
